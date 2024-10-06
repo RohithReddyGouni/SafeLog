7893b98bf96c5cb7d41972c2d8a2b3ed076d86c5
Chandrashekar Goud <ckalali@depaul.edu>
2024-10-05 23:43:49 -0500

    Added basic structure and login component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Safety Log Application</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
