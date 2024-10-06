45cb8e1b1b0ce122112d0b550278518cc3df87a7
RohithReddy Gouni <grohithreddy9999@gmail.com>
2024-10-05 23:16:50 -0500

    Removed unnecessary files: HELP.md and mvnw.cmd

---
+++ backend/src/main/java/com/safelog/SafeLogApplication.java
@@ -0,0 +1,13 @@
+package com.safelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeLogApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/safelog/SafeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
